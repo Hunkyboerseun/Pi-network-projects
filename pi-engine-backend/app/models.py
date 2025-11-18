0e03706cb6d24992d7557a0f44e1916cbec12c10
Hunkyboerseun <116794874+Hunkyboerseun@users.noreply.github.com>
2025-11-18 17:37:55 +0200

    Create models.py

---
+++ pi-engine-backend/app/models.py
@@ -0,0 +1,6 @@
+class EngineState:
+    def __init__(self):
+        self.pi_price = 40.0          # starting price in USD
+        self.pi_supply = 300_000_000  # initial circulating supply
+
+state = EngineState()
