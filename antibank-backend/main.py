fb2c787034e8d24d1690d7ff954689cb3bdef898
Hunkyboerseun <116794874+Hunkyboerseun@users.noreply.github.com>
2025-11-19 09:31:31 +0200

    Update main.py

--- antibank-backend/main.py
+++ antibank-backend/main.py
@@ -1,14 +1,2 @@
-from fastapi import FastAPI
-from app.routes.balance import router as balance_routes
-from app.routes.transactions import router as transaction_routes
-from app.routes.system import router as system_routes
-
-app = FastAPI(title="ANTIBANK Backend API")
-
-app.include_router(balance_routes)
-app.include_router(transaction_routes)
-app.include_router(system_routes)
-
-@app.get("/")
-def home():
-    return {"message": "Welcome to ANTIBANK Backend API"}
+from app.routes.pi import router as pi_routes
+app.include_router(pi_routes)
