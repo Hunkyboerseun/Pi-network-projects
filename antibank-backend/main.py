9ae5e8a7dca29dbe575053e389da649e9eabdbfd
Hunkyboerseun <116794874+Hunkyboerseun@users.noreply.github.com>
2025-11-20 07:44:17 +0200

    Update main.py

--- antibank-backend/main.py
+++ antibank-backend/main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="ANTIBANK Backend")
 
-# CORS so frontend can access backend
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -14,4 +13,3 @@ app.add_middleware(
 @app.get("/")
 def home():
     return {"status": "OK", "message": "ANTIBANK backend running"}
-
